931171c797ca7e521826b6b43a6b513aef07ba1e
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-16 15:13:01 +0330

    recipe page added

--- core/home/views.py
+++ core/home/views.py
@@ -6,6 +6,36 @@ from django.contrib.auth import login , authenticate , logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-def home(request):
 
-    return render(request , 'home/index.html' , context={})
+
+@login_required(login_url='/login/')
+def recipes(request):
+    if request.method == 'POST' :
+        data = request.POST
+        day = data.get('day')
+        name = data.get('name')
+        description = data.get('description')
+        Recipe.objects.create(
+            day = day , 
+            name = name , 
+            description = description
+        )
+        return redirect('/')
+
+    query_set = Recipe.objects.all()
+    if request.GET.GET('search'):
+        query_set = query_set.filter(
+            day__icontains=request.GET.get('search'))
+        
+
+    context = {
+        'recipes' : query_set
+    }
+
+    return render(request , 'home/recipe.html' , context)
+
+
+
+
+
+
